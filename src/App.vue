8e760f012e19f6ec3cee2aa26a31329941d1e308
water <695381719@qq.com>
2020-07-24 18:27:41 +0800

    add reset.css and mixins.styl

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,46 @@
 <template>
   <div id="app">
-    <HelloWorld msg="HelloWorld"/>
+    <el-container>
+      <el-header>header 菜单栏</el-header>
+      <el-container>
+        <el-aside>aside</el-aside>
+        <el-main>
+          <VTodolist></VTodolist>
+        </el-main>
+      </el-container>
+      <el-footer>footer 状态栏</el-footer>
+    </el-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VTodolist from './components/VTodolist.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    VTodolist
   }
 }
 </script>
 
-<style>
-
+<style scoped>
+.el-header, .el-footer {
+  background-color: #b3c0d1;
+  color:#333;
+  text-align: center;
+  line-height:60px;
+}
+.el-aside{
+  background-color: #b3c0d1;
+  color:#333;
+  text-align: center;
+  line-height: 200px;
+}
+.el-main{
+  background-color:#e9eef3;
+  color:#333;
+  text-align:center;
+  line-height: 160px;
+}
 </style>
---
+++ src/components/VTodolist.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    todolist
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VTodolist',
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  
+</style>
